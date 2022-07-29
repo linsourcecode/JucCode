90dec1d0920957e46ebfc1167d6538c094f8b54e
WIN-RT09V4GCT0I\Administrator <1679569188@qq.com>
2022-07-29 09:09:30 +0800

    使用原生的多线程创建方式

---
+++ ThreadCreate/src/com.example/ThreadCreate.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/***
+ * 线程创建的三种方法
+ * 一 创建一个或者继承Thread类
+ * 二 继承Runnable
+ * 三 使用Callable结合Future异步任务使用
+ *
+ * */
+public class ThreadCreate {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new Thread(()->{
+            System.out.println(Thread.currentThread().getName());
+        }).start();
+        new MyThread().run();
+        new Thread(new MyThread2()).run();
+        ExecutorService executorService2= Executors.newCachedThreadPool();
+
+        Future<String> future = executorService2.submit(new MyThread3());
+        future.isDone();
+        System.out.println(future.get());
+        executorService2.shutdown();
+
+
+    }
+}
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+class MyThread2 extends Thread{
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+class MyThread3 implements Callable<String>{
+
+    @Override
+    public String call() throws Exception {
+        return "Success";
+    }
+}
